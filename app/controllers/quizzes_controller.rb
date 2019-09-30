4789c1602e2e7925ad4601ad6a1b3d7270a437da
github username <github email address>
2019-09-30 12:09:14 -0400

    quiz stuff

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,2 +1,14 @@
 class QuizzesController < ApplicationController
+
+    def show 
+        render :show
+    end 
+    
+
+    private 
+
+    def quiz_params 
+        params.require(:quiz).permit(:user_id, :friendship_id, :name)
+    end 
+
 end
