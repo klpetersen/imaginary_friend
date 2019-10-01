db6d37e2043877a4fa1ccbf6c52b43d7e2234514
castagnaTM <castagna.tm@gmail.com>
2019-10-01 16:19:31 -0400

    Updated Login System

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,16 +4,23 @@ class UsersController < ApplicationController
     end
     
     def show
-      render :show
+        @user = User.find(params[:id])
     end
 
     def create
+        # create new user
+        # add and error handling validations later
         @user = User.create(user_params)
+
         redirect_to user_path(@user)
     end
 
     def user_params
-        params.require(:user).permit(:id, :user_name, :password, :password_confirmation, :email, :name)
+        params.require(:user).permit(:user_name, :password, :password_confirmation, :email, :name)
+    end
+
+    def new
+
     end
 
 end
