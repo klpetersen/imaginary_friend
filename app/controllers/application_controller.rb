593471d8ec3ccf0fa43dba7044ad08cc7899d7e7
castagnaTM <castagna.tm@gmail.com>
2019-10-01 13:26:10 -0400

    removed all quiz materials

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,25 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception
-    before_action :authenticate
-    
-    def authenticate
-        if logged_in?
-            current_user
-        else
-            flash[:error] = "Woooaaahh, slow down there, kiddo! You gotta login to see this page!"
-            redirect_to login_path
-        end
-    end
+protect_from_forgery with: :exception
+helper_method :logged_in?, :unauthorized
 
-    def current_user
-        @user = User.find_by(id: session[:user_id])
-    end
+  def current_user
+    @user = User.find_by(id: session[:user_id])
+  end
 
-    def logged_in?
-        !!current_user
+  def logged_in?
+    !!current_user
+  end
+
+  def authenticate
+    if logged_in?
+      current_user
+    else
+      flash[:error] = "You must be logged in to see that"
+      redirect_to login_path
     end
+  end
+
+  def unauthorized
+    redirect_to login_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get '/quizzes', to: 'quizzes#show'
-
   root to: 'users#welcome'
   get '/login', to: 'sessions#new', as: :login
   post '/login', to: 'sessions#create'
-  get '/logout', to: 'sessions#destroy'
+  #get '/logout', to: 'sessions#destroy', as: :logout
   #post '/login', to: 'sessions#authenticate'
-  resources :users, only: [:create, :new, :show]
+  resources :users, only: [:new, :create, :show]
 end
