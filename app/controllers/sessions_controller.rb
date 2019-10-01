9ad021d744999f9ae77fe2f3a54588fc36c34ed4
castagnaTM <castagna.tm@gmail.com>
2019-10-01 17:16:07 -0400

    updating

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
   
     def create
       @user = User.find_by(email: params[:user][:email])
+      byebug
   
       if @user && @user.authenticate(user_params)
         session[:user_id] = @user.id
