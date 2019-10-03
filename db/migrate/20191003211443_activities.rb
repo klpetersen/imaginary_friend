65aa437636785201b10483d7f8a92569e2ec4bd6
castagnaTM <castagna.tm@gmail.com>
2019-10-03 17:56:03 -0400

    fixed seeds

--- db/migrate/20191003211443_activities.rb
+++ db/migrate/20191003211443_activities.rb
@@ -1,8 +1,8 @@
-class CreateActivities < ActiveRecord::Migration[6.0]
+class Activities < ActiveRecord::Migration[6.0]
   def change
     create_table :activities do |t|
       t.string :name
-      t.string :type
+      t.string :act_type
       t.integer :pref_value
 
       t.timestamps
